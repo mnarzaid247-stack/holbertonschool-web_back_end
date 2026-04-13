7dbe9253a4b31f604b237a867d4d0920d1b4910f
manar <mnarzaid247@gmail.com>
2026-04-13 14:34:34 +0300

    update

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 '''adding two floats'''
+
 def add(a: float, b: float) -> float:
     '''the def'''
     return a + b
--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 '''from float to string'''
+
 def to_str(n: float) -> str:
     '''the def'''
     return str(n)

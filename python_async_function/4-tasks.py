6f1bb7fade9c2b5c9bf3371cfafabd43fc41dd66
manar <mnarzaid247@gmail.com>
2026-04-14 15:02:30 +0300

    update

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""Async tasks execution module"""
+
+
 import asyncio
 from typing import List
 
@@ -6,6 +9,8 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Create an asynchronous coroutine that spawns tasks
+    and returns delays in ascending order."""
     tasks = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
 

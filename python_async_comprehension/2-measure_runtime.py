154a42d823a0b2120321bdc97a6ff42618dd200e
manar <mnarzaid247@gmail.com>
2026-04-16 22:00:28 +0300

    update

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -15,5 +15,5 @@ async def measure_runtime() -> float:
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
-)
+    )
     return time.perf_counter() - start

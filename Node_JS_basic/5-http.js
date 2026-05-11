e35797ec5a0516bfdc08123bce2b0a5bae98cd03
manar <mnarzaid247@gmail.com>
2026-05-11 20:08:05 +0300

    update

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.end('This is the list of our students');
+  } else {
+    res.end('Not found');
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
---
+++ Node_JS_basic/README.md
@@ -0,0 +1,1 @@
+this projecy is about node.js

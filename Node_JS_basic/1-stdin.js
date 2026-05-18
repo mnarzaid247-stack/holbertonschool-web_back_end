f5dcb6d0c8b1d090dfedfc4f909d6ced1f735b47
manar <mnarzaid247@gmail.com>
2026-05-18 08:55:38 +0300

    update

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,11 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-  console.log(`Your name is: ${data.toString().trim()}`);
+  process.stdout.write(`Your name is: ${data.toString().trim()}\n`);
 });
 
 process.on('exit', () => {
   console.log('This important software is now closing');
 });
+
+process.stdin.resume();

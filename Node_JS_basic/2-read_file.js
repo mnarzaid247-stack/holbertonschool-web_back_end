03d05b0b1c821d1d879f2829b20fc4a395d215de
manar <mnarzaid247@gmail.com>
2026-05-18 09:03:19 +0300

    update

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -4,15 +4,13 @@ function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8');
 
-    const lines = data
-      .split('\n')
-      .filter((line) => line.trim() !== '');
-
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
     const students = lines.slice(1);
 
     console.log(`Number of students: ${students.length}`);
 
     const fields = {};
+    const fieldOrder = [];
 
     students.forEach((student) => {
       const parts = student.split(',');
@@ -21,16 +19,17 @@ function countStudents(path) {
 
       if (!fields[field]) {
         fields[field] = [];
+        fieldOrder.push(field);
       }
 
       fields[field].push(firstname);
     });
 
-    for (const field in fields) {
+    fieldOrder.forEach((field) => {
       console.log(
         `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
       );
-    }
+    });
   } catch (err) {
     throw new Error('Cannot load the database');
   }
